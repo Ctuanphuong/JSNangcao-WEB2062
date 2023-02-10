5906b7be5fb1f7c3d71fb88c4450a6543cc776d6
Ctuanphuong18 <Ctuanphuong18@gmail.com>
2023-02-10 11:42:36 +0700

    upload section 1-8

---
+++ Section1_8/js/main.js
@@ -0,0 +1,63 @@
+/*Test:  Tính chỉ số BMI 
+
+const massMark = 70;
+const heightMark = 1.7;
+const massJohn = 80;
+const heightJohn = 1.85;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn);
+console.log(markHigherBMI);
+*/
+
+/* Tìm người có điểm cao nhất
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log("Dolphin win the trophy");
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+  console.log("Koalas win the trophy");
+} else if (
+  scoreKoalas === scoreDolphins &&
+  scoreDolphins >= 100 &&
+  scoreKoalas >= 100
+) {
+  console.log("Both win the trophy");
+} else {
+  console.log("No one wins the trophy");
+}
+*/
+
+// /* Toán tử 3 ngôi
+// const age = 20;
+// const drink =
+//   age >= 18 ? "Be able to drive motobike" : "Don't allowed to drive";
+// console.log(drink);
+// console.log(
+//   `I ${age >= 18 ? "be able to drive motobike" : "Don't allowed to drive"}`
+// );
+// */
+
+/* Tính tiền bo nhà hàng:
+const bill = 301;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(
+  `The bill was ${bill} and tip was ${tip}, the total value ${bill + tip}`
+);
+
+*/
+/*Buổi 2:
+const handleInfo = (age) => age;
+console.log(handleInfo(20));
+
+const User = {
+  id: 1,
+  name: "User",
+  age: 19,
+};
+*/
